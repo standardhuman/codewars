95983c24011277398f16e545eb6d0fefcc0e2246
Brian Cline <standardhuman@gmail.com>
2017-04-17 22:03:10 -0700

    first commit

---
+++ accum.js
@@ -0,0 +1,21 @@
+function accum(s) {
+	// your code
+	const split = s.split('').for(let letter in split){
+		// determine the index number of letter
+		// need to use keys here
+		
+		// set letter to uppercase
+		let upper = letter.toUpperCase()
+		// get lower case version of letter
+		let lower = letter.toLowerCase()
+		// add lower to upper index number of times
+		let combined = upper + lower
+		console.log(combined);
+}
+	return split;
+//   split s
+//  push(print each index value toUpperCase + print x index value)
+// join
+}
+
+console.log(accum("ZpglnRxqenU"));
