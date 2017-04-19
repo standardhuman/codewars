fed4e241879e3c270f749c0e055e7078bb341830
Brian Cline <standardhuman@gmail.com>
2017-04-18 23:01:15 -0700

    working on keys

--- accum.js
+++ accum.js
@@ -1,16 +1,19 @@
 function accum(s) {
 	// your code
-	const split = s.split('').for(let letter in split){
+	const split = s.split('')
+	let index = Object.keys(split)
+	console.log(index);
+
+  for(let letter in split){
 		// determine the index number of letter
 		// need to use keys here
-		
 		// set letter to uppercase
 		let upper = letter.toUpperCase()
 		// get lower case version of letter
 		let lower = letter.toLowerCase()
 		// add lower to upper index number of times
 		let combined = upper + lower
-		console.log(combined);
+		// console.log(combined);
 }
 	return split;
 //   split s
@@ -19,3 +22,9 @@ function accum(s) {
 }
 
 console.log(accum("ZpglnRxqenU"));
+
+var arr = ['a', , 'c'];
+var sparseKeys = Object.keys(arr);
+var denseKeys = [...arr.keys()];
+console.log(sparseKeys); // ['0', '2']
+console.log(denseKeys);  // [0, 1, 2]
